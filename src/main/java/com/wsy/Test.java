37cc219551685d1abeef299d52164788e0f84f97
wangsiyue <wangsiyue@dongfangfuli.com>
2022-02-26 11:35:22 +0800

    手写spring

--- src/main/java/com/wsy/Test.java
+++ src/main/java/com/wsy/Test.java
@@ -6,19 +6,28 @@ import com.wsy.service.UserService;
 public class Test {
     public static void main(String[] args) {
 
-        WsyApplicationContext applicationContext = new WsyApplicationContext(AppConfig.class);
+//        WsyApplicationContext applicationContext = new WsyApplicationContext(AppConfig.class);
 
         UserService userService = null;
+        int a = 0;
         try {
 //            userService = (UserService) applicationContext.getBean("userService");
 //            System.out.println(applicationContext.getBean("userService"));
 //            System.out.println(applicationContext.getBean("userService"));
-            System.out.println(applicationContext.getBean("orderService"));
-            UserService userService1 = (UserService)applicationContext.getBean("userService");
-            userService1.test();
+//            System.out.println(applicationContext.getBean("orderService"));
+//            UserService userService1 = (UserService)applicationContext.getBean("userService");
+//            userService1.test();
+
+            a++;
+            System.out.println(a);
+            throw new Exception();
         } catch (Exception e) {
             e.printStackTrace();
+            System.out.println(a);
+            a++;
+            System.out.println(a);
         }
+        System.out.println(a);
 //        userService.test();
     }
 }
