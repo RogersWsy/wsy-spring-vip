0de7d6107da9063e87959db4a62880339ffe77ab
wangsy2 <wangsy2@fuiou.com>
2022-07-21 22:44:09 +0800

    依赖注入

--- src/main/java/com/wsy/Test5.java
+++ src/main/java/com/wsy/Test5.java
@@ -4,18 +4,25 @@ import com.wsy.service.User;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.annotation.AnnotatedBeanDefinitionReader;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ClassPathBeanDefinitionScanner;
 
 public class Test5 {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+//        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.refresh();
 //        context.register();
 
 //        AnnotatedBeanDefinitionReader reader = new AnnotatedBeanDefinitionReader(context);
 //        reader.register(User.class);
 //        System.out.println(context.getBean("user"));
-        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(context);
-        int i = reader.loadBeanDefinitions("spring.xml");
 
+//        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(context);
+//        int i = reader.loadBeanDefinitions("spring.xml");
+//        System.out.println(context.getBean("user"));
+
+        ClassPathBeanDefinitionScanner scanner = new ClassPathBeanDefinitionScanner(context);
+        scanner.scan("com.wsy");
         System.out.println(context.getBean("user"));
 
     }
