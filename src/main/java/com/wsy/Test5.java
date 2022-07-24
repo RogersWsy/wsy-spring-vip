68669754214f552ad6d1606da1653eef997d920e
wangsy2 <wangsy2@fuiou.com>
2022-07-24 09:06:42 +0800

    依赖注入

--- src/main/java/com/wsy/Test5.java
+++ src/main/java/com/wsy/Test5.java
@@ -1,6 +1,9 @@
 package com.wsy;
 
 import com.wsy.service.User;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.annotation.AnnotatedBeanDefinitionReader;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -20,10 +23,16 @@ public class Test5 {
 //        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(context);
 //        int i = reader.loadBeanDefinitions("spring.xml");
 //        System.out.println(context.getBean("user"));
+//
+//        ClassPathBeanDefinitionScanner scanner = new ClassPathBeanDefinitionScanner(context);
+//        scanner.scan("com.wsy");
+//        System.out.println(context.getBean("user"));
 
-        ClassPathBeanDefinitionScanner scanner = new ClassPathBeanDefinitionScanner(context);
-        scanner.scan("com.wsy");
-        System.out.println(context.getBean("user"));
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        AbstractBeanDefinition beanDefinition = BeanDefinitionBuilder.genericBeanDefinition().getBeanDefinition();
+        beanDefinition.setBeanClass(User.class);
 
+        beanFactory.registerBeanDefinition("user",beanDefinition);
+        System.out.println(beanFactory.getBean("user"));
     }
 }
