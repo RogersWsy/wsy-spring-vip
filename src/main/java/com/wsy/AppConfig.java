67d2cb598acfba017e3330a1a543f1a13968b8c4
wangsy2 <wangsy2@fuiou.com>
2022-08-03 23:54:17 +0800

    国际化

--- src/main/java/com/wsy/AppConfig.java
+++ src/main/java/com/wsy/AppConfig.java
@@ -1,9 +1,18 @@
 package com.wsy;
 
 import com.spring.ComponentScan;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @ComponentScan("com.wsy.service")
 public class AppConfig {
 
+    @Bean
+    public MessageSource messageSource{
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
+    }
 
 }
