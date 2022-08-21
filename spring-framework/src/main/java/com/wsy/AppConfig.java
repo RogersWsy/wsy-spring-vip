ae52eb63d975156aec5f0bdeabbaf597aff07c5d
wangsiyue <wangsiyue@dongfangfuli.com>
2022-08-21 16:21:28 +0800

    发布事件

---
+++ spring-framework/src/main/java/com/wsy/AppConfig.java
@@ -0,0 +1,31 @@
+package com.wsy;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+@ComponentScan("com.wsy")
+@PropertySource("classpath:spring.properties")
+public class AppConfig {
+
+    @Bean
+    public MessageSource messageSource(){
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
+    }
+
+    @Bean
+    public ApplicationListener applicationListener(){
+        return new ApplicationListener() {
+            @Override
+            public void onApplicationEvent(ApplicationEvent event) {
+                System.out.println("接收到了一个事件"+event);
+            }
+        };
+    }
+}
